de1912819c882d862b41bb474b0ec7aefc7eb2fc
Mohamed Sameh <ms7100100@gmail.com>
2024-05-13 19:31:25 +0200

    some changes on the mealcard

--- src/component/Nav/Nav.jsx
+++ src/component/Nav/Nav.jsx
@@ -8,53 +8,6 @@ const Nav = () => {
   return (
     <header className="header">
       <nav className="nav">
-        {/* <div className="nav__wrapper">
-          <div className="logo">
-            <img src={Logo} alt="logo" />
-            <h1>
-              Pizza<span className="highlight">Day</span>
-            </h1>
-          </div>
-
-          <div className="navigation">
-            <ul className="nav__menu">
-              <li className="nav-item">
-                <a href="#home" className="nav__link">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#menu" className="nav__link">
-                  Menü
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#about" className="nav__link">
-                  Reservation
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#blog" className="nav__link">
-                  Angebote & Gustscheine{" "}
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#blog" className="nav__link">
-                  Kontakt
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="nav-buttons">
-            <button>Login</button>
-            <FaCartShopping className="cart" />
-          </div>
-
-          <span className="mobile__menu">
-            <i className="ri-menu-line"></i>
-          </span>
-        </div> */}
-
         <div className="logo">
           <img src={Logo} alt="logo" />
           <h1>
