f87dc8b5c5064b8ecb7c84b907bbd04682144abc
Mohamed Sameh <ms7100100@gmail.com>
2024-05-13 19:03:32 +0200

    customize the navbar and make it responsive

--- src/component/Nav/Nav.jsx
+++ src/component/Nav/Nav.jsx
@@ -1,45 +1,110 @@
 // import React from 'react'
-import "./Nav.css"
-import Logo from "../../assets/85a30340-f824-4e4a-b2a5-c5d808affecc.jpg"
+import "./Nav.css";
+import Logo from "../../assets/85a30340-f824-4e4a-b2a5-c5d808affecc.jpg";
+import { FaCartShopping } from "react-icons/fa6";
+import { CiMenuBurger } from "react-icons/ci";
+
 const Nav = () => {
   return (
-      <header className="header">
-          <nav className="nav">
-                  <div className="nav__wrapper">
-                      <div className="logo">
-                            <img src={Logo} alt="logo"/>
-                          <h1>Pizza<span className="highlight">Day</span></h1>
-                      </div>
+    <header className="header">
+      <nav className="nav">
+        {/* <div className="nav__wrapper">
+          <div className="logo">
+            <img src={Logo} alt="logo" />
+            <h1>
+              Pizza<span className="highlight">Day</span>
+            </h1>
+          </div>
 
-                      <div className="navigation">
-                          <ul className="nav__menu">
-                              <li className="nav__item">
-                                  <a href="#home" className="nav__link">Home</a>
-                              </li>
-                              <li className="nav__item">
-                                  <a href="#menu" className="nav__link">Menü</a>
-                              </li>
-                              <li className="nav__item">
-                                  <a href="#about" className="nav__link">Reservation</a>
-                              </li>
-                              <li className="nav__item">
-                                  <a href="#blog" className="nav__link">Angebote & Gustscheine </a>
-                              </li>
-                              <li className="nav__item">
-                                  <a href="#blog" className="nav__link">Kontakt</a>
-                              </li>
-                          </ul>
-                      </div>
-                      <div className="nav-buttons"> 
-                        <button>Login</button>
-                        
-                      </div>
-                
-                      <span className="mobile__menu"><i className="ri-menu-line"></i></span>
-                  </div>
-          </nav>
-      </header>
-  )
-}
+          <div className="navigation">
+            <ul className="nav__menu">
+              <li className="nav-item">
+                <a href="#home" className="nav__link">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#menu" className="nav__link">
+                  Menü
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#about" className="nav__link">
+                  Reservation
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#blog" className="nav__link">
+                  Angebote & Gustscheine{" "}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#blog" className="nav__link">
+                  Kontakt
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="nav-buttons">
+            <button>Login</button>
+            <FaCartShopping className="cart" />
+          </div>
 
-export default Nav
+          <span className="mobile__menu">
+            <i className="ri-menu-line"></i>
+          </span>
+        </div> */}
+
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+          <h1>
+            <span className="highlight">wagon</span>pizza
+          </h1>
+        </div>
+
+        <div className="navigation">
+          <ul className="nav-links">
+            <li className="nav-item">
+              <a href="#home" className="nav__link">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#menu" className="nav__link">
+                Menü
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#about" className="nav__link">
+                Reservation
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#blog" className="nav__link">
+                Angebote & Gustscheine{" "}
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#blog" className="nav__link">
+                Kontakt
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="nav-buttons">
+          <button>Login</button>
+          <div className="cart-container">
+            <FaCartShopping className="cart" />
+            <span className="quantity">0</span>
+          </div>
+          <div className="menu-btn">
+            <CiMenuBurger className="menu"/>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
